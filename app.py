49585ec2e31385e7c65b001e07aa3ad1c269414c
juliusdabre <juliusdabre85@gmail.com>
2025-04-12 16:08:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 
 df = pd.read_excel("GGI Jan 2025.xlsx")
 st.set_page_config(page_title="NextRise - Property Investment Dashboard", layout="wide")
-st.image("logo.png", width=200)
+st.image("logo.png", width=500)
 st.title("NextRise")
 
 st.sidebar.header("🔍 Filter Suburb Data")
